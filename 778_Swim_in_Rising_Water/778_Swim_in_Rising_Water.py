ab480e5026a9fe6f47bdf83c573a780ce5180762
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-08-23 04:25:37 -0400

    Update 778_Swim_in_Rising_Water.py

--- 778_Swim_in_Rising_Water/778_Swim_in_Rising_Water.py
+++ 778_Swim_in_Rising_Water/778_Swim_in_Rising_Water.py
@@ -1,3 +1,47 @@
+# BFS + binary search
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        def bfs(grid, time):
+            
+            visited  = set((0,0))
+            directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+            cur_level = max(grid[0][0], time)
+            que = deque([(cur_level,0,0)])
+            
+            while que:
+                cur_level, cor_x, cor_y = que.popleft()
+                
+                if cor_x == len(grid)-1  and cor_y == len(grid)-1:
+                    return True
+                
+                for i in directions:
+                    n_x = cor_x + i[0]
+                    n_y = cor_y + i[1]
+                    
+                    
+                    if 0 <= n_x < len(grid) and 0 <= n_y < len(grid) and (n_x,n_y) not in visited:
+                        new_level = max(grid[n_x][n_y], time)
+                        if new_level==cur_level:
+                            visited.add((n_x,n_y))
+                            que.append((new_level,n_x,n_y))
+                    
+                                 
+        left = 0
+        right = (len(grid))**2
+        ans = None
+        while left <= right:
+            m = (left+right)//2
+            
+            if bfs(grid, m):
+                ans = m
+                right = m-1
+            else:
+                left = m+1
+                
+        return ans
+                
+            
+# not sure if this is fully BFS
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         heap = []
