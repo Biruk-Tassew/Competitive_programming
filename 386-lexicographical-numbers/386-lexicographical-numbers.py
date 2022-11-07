c049424e4d2f4f662a62a2d2287e07bf2f39d77d
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-07 13:20:04 +0300

    Time: 665 ms (11.32%), Space: 32.2 MB (5.18%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.py
+++ 386-lexicographical-numbers/386-lexicographical-numbers.py
@@ -1,5 +1,44 @@
+class TrieNode:
+    def __init__(self, dig):
+        self.dig = dig
+        self.is_end = False
+        self.children = {}
+        
 class Solution:
+    def __init__(self):
+        self.root = TrieNode("")
+        
+    def insert(self, num):
+        node = self.root
+        
+        for i in num:
+            if i in node.children:
+                node = node.children[i]
+            else:
+                n_node = TrieNode(i)
+                node.children[i] = n_node
+                node = n_node
+            
+        node.is_end = True
+        
+    def dfs(self, node, pre):
+        if node.is_end:
+            self.res.append(pre)
+            
+        for i in node.children.values():
+            self.dfs(i, pre + i.dig)    
+            
     def lexicalOrder(self, n: int) -> List[int]:
-        s = list(map(str, [i for i in range(1, n+1)]))
-                 
-        return sorted(s)
+        for i in range(1, n+1):
+            self.insert(str(i))
+            
+        self.res = []
+        self.dfs(self.root, "")
+        return self.res
+        
+         
+        
+                    
+                    
+            
+    
