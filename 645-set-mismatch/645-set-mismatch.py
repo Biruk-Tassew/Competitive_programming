fc5a4b6527a0eeef955d3ed0431554fc59913047
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-09 16:41:01 +0300

    Time: 485 ms (45.96%), Space: 15.9 MB (22.26%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.py
+++ 645-set-mismatch/645-set-mismatch.py
@@ -1,16 +1,8 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        store = defaultdict(lambda :0)
-        res = [0, 0]
+        uni_sum = sum(set(nums))
+        given_sum = sum(nums)
+        whole_sum = len(nums)*(len(nums)+1)//2
         
-        for i in nums:
-            store[i] += 1
-            if store[i] == 2:
-                res[0] = i
-         
-        for i in range(1, len(nums)+1):
-            if not store[i]:
-                res[1] = i
-                
-        return res
+        return [given_sum-uni_sum, whole_sum-uni_sum]
             
