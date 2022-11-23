ee26ec9d8c65f7a7ad87041a9455f432e58386c1
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-23 20:14:52 +0300

    Time: 263 ms (17.03%), Space: 14 MB (36.06%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,31 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        for i in range(len(board)):
-            temp_board = []
-            for j in board[i]:
-                if j.isdigit():
-                    temp_board.append(j)
-            if len(set(temp_board)) != len(temp_board):
-                return False
-            
+        seen = set()
         
-        for j in zip(*board):
-            temp_board = []
-            for k in j:
-                if k.isdigit():
-                    temp_board.append(k)
-            if len(set(temp_board)) != len(temp_board):
-                return False
-        
-        for row in range(0, 7, 3):
-            for col in range(0, 7, 3):
-                square = [board[x][y] for x in range(row, row + 3) for y in range(col, col + 3)]
-               
-                square = [i for i in square if i != '.']
-                if len(set(square)) != len(square):
-                    return False
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] != ".":
+                    if board[row][col]+"in row"+str(row) in seen or\
+                       board[row][col]+"in col"+str(col) in seen or\
+                       board[row][col]+str(row//3)+str(col//3) in seen:
+                        return False
+                    seen.add(board[row][col]+"in row"+str(row))
+                    seen.add(board[row][col]+"in col"+str(col))
+                    seen.add(board[row][col]+str(row//3)+str(col//3))
+                    
         return True
+                
             
         
         
