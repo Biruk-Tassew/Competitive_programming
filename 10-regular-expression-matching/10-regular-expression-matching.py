f0fd95e338459503bac7ef176d1b6553d36f6f83
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-23 20:30:04 +0300

    Time: 120 ms (41.97%), Space: 14 MB (62.52%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -1,24 +1,23 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        self.s = s 
+        self.s = s
         self.p = p
-        self.memo = defaultdict(bool)
+        self.memo = defaultdict(lambda:-1)
         return self.solve(0, 0)
-        
-        
-    def solve(self, s_idx, p_idx):
+    
+    def solve(self, p_idx, s_idx):
         if p_idx == len(self.p):
-            return s_idx == len(self.s)
+            return len(self.s) == s_idx
         
-        if self.memo[(p_idx, s_idx)]:
+        if self.memo[(s_idx, p_idx)] != -1:
             return self.memo[(s_idx, p_idx)]
         
         matching = s_idx < len(self.s) and self.p[p_idx] in {self.s[s_idx], "."}
         
         if p_idx < len(self.p)-1 and self.p[p_idx+1] == "*":
-            ans = (matching and self.solve(s_idx+1, p_idx)) or self.solve(s_idx, p_idx+2)
+            ans = (matching and self.solve(p_idx, s_idx+1) or (self.solve(p_idx+2, s_idx)))
         else:
-            ans = matching and self.solve(s_idx+1, p_idx+1)
+            ans = matching and self.solve(p_idx+1, s_idx+1)
             
         self.memo[(s_idx, p_idx)] = ans
         return self.memo[(s_idx, p_idx)]
