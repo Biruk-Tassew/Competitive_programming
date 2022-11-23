a2f89116fb67c415df14e5ccb267fc57cc6a9586
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-23 19:09:39 +0300

    Time: 328 ms (100.00%), Space: 24.8 MB (91.11%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -2,8 +2,27 @@ class Solution:
     def minDifference(self, nums: List[int]) -> int:
         if len(nums) <= 3:
             return 0
+        elif len(nums) <= 7:
+            return self.calc_diff(nums, True)
         
-        nums.sort()
+        min_heap = nums
+        heapq.heapify(min_heap)
+        max_heap = [-num for num in nums]
+        heapq.heapify(max_heap)
+        four_min_max = [0]*8
+        temp = []
+        for i in range(8):
+            if i < 4:
+                four_min_max[i] = heapq.heappop(min_heap)
+            else:
+                temp.append(-heapq.heappop(max_heap))
+        
+        four_min_max += temp[::-1]
+        return self.calc_diff(four_min_max, False)
+       
+    def calc_diff(self, nums, flag):
+        if flag:
+            nums.sort()
         min_diff = float('inf')
         
         # change the three biggest elements
