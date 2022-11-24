fbcb319e990e9f72691d5a405f22f9326c73723b
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-24 16:02:04 +0300

    Time: 220 ms (72.09%), Space: 16.5 MB (88.26%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,20 +1,31 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = [[0]*(len(word1)+1) for _ in range(len(word2)+1)]
-        
-        for col in range(len(word1)+1):
-            dp[0][col] = col
-        for row in range(len(word2)+1):
-            dp[row][0] = row
+        self.word1 = word1
+        self.word2 = word2
+        self.memo = defaultdict(lambda :-1)
+        return self.solve(0, 0)
+    
+    def solve(self, idx_one, idx_two):
+        if idx_one == len(self.word1):
+            if idx_two == len(self.word2):
+                return 0
+            else:
+                return len(self.word2)-idx_two
             
-        for row in range(1, len(word2)+1):
-            for col in range(1, len(word1)+1):
-                if word1[col-1] == word2[row-1]:
-                    dp[row][col] = dp[row-1][col-1]
-                else:
-                    insert = dp[row][col-1] + 1
-                    delete = dp[row-1][col] + 1
-                    replace = dp[row-1][col-1] + 1
-                    dp[row][col] = min(insert, delete, replace)
-                            
-        return dp[-1][-1]
+        if idx_two == len(self.word2):
+            return len(self.word1)-idx_one
+        
+        if self.memo[(idx_one, idx_two)] != -1:
+            return self.memo[(idx_one, idx_two)]
+      
+        
+        if self.word1[idx_one] == self.word2[idx_two]:
+            ans = self.solve(idx_one+1, idx_two+1)
+        else:
+            insert = self.solve(idx_one, idx_two+1)+1
+            delete = self.solve(idx_one+1, idx_two)+1
+            replace = self.solve(idx_one+1, idx_two+1)+1
+            ans = min(insert, delete, replace)
+            
+        self.memo[(idx_one, idx_two)] = ans
+        return self.memo[(idx_one, idx_two)]
