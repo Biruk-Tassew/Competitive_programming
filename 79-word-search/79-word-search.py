70293b899a57d4f0ff63e60f790c8e8078b21e88
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-24 08:37:38 +0300

    Time: 6694 ms (34.97%), Space: 14 MB (52.49%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -5,22 +5,23 @@ class Solution:
         self.found = False
         for row in range(len(board)):
             for col in range(len(board[0])):
-                self.solve(row, col, 0, word)
-                if self.found:
+                if self.solve(row, col, 0, word):
                     return True
         return False
     
     def solve(self, row, col, cur_idx, word):
         if cur_idx == len(word):
-            self.found = True
-            return
+            return True
         
+        res = False
         if 0 <= row < len(self.board) and 0 <= col < len(self.board[0]) and self.board[row][col] == word[cur_idx]:
             temp_char = self.board[row][col]
             self.board[row][col] = -1
             
             for i in self.directions:
                 
-                    self.solve(row+i[0], col+i[1], cur_idx+1, word)
+                    res = res or self.solve(row+i[0], col+i[1], cur_idx+1, word)
                     
             self.board[row][col] = temp_char
+            
+        return res
