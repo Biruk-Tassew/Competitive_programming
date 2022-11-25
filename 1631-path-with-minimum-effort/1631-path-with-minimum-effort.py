8eea8f2c9d8a798600363434987034f0e84c7d18
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-25 21:44:17 +0300

    Time: 2055 ms (53.82%), Space: 15.3 MB (76.26%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         all_efforts = [[float('inf')]*len(heights[0]) for _ in range(len(heights))]
         all_efforts[0][0] = 0
         heap = [(0, 0, 0)]
@@ -9,16 +9,15 @@ class Solution:
         while heap:
             cur_effort, row, col = heapq.heappop(heap)
             
-            if row == len(heights)-1 and col == len(heights[0])-1:
+            if row==len(heights)-1 and col==len(heights[0])-1:
                 return cur_effort
             
             for i in directions:
-                n_r = row + i[0]
-                n_c = col + i[1]
+                n_r = row+i[0]
+                n_c = col+i[1]
                 
                 if 0 <= n_r < len(heights) and 0 <= n_c < len(heights[0]):
-                    max_effort = max(cur_effort, abs(heights[row][col]-heights[n_r][n_c]))
-                    if all_efforts[n_r][n_c] > max_effort:
-                        
-                        all_efforts[n_r][n_c] = max_effort
-                        heapq.heappush(heap, (max_effort, n_r, n_c))
+                    n_effort = max(cur_effort, abs(heights[row][col]-heights[n_r][n_c]))
+                    if n_effort < all_efforts[n_r][n_c]:
+                        all_efforts[n_r][n_c] = n_effort
+                        heapq.heappush(heap, (n_effort, n_r, n_c))
