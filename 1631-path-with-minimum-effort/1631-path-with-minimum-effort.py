d37bda1e14c725d6490baef2c7e7a11d9da63aa7
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-25 21:33:02 +0300

    Time: 1774 ms (65.69%), Space: 15.4 MB (70.30%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,42 +1,24 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        abs_max = 0
-        abs_min = float('inf')
-        for row in range(len(heights)):
-            for col in range(len(heights[0])):
-                abs_min = min(abs_min, heights[row][col])
-                abs_max = max(abs_max, heights[row][col])
-                
-        left = 0
-        right = abs_max-abs_min
-        while left < right:
-            mid = (left+right)//2
+        
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        all_efforts = [[float('inf')]*len(heights[0]) for _ in range(len(heights))]
+        all_efforts[0][0] = 0
+        heap = [(0, 0, 0)]
+        
+        while heap:
+            cur_effort, row, col = heapq.heappop(heap)
             
-            if self.bfs(0, 0, mid, heights):
-                right = mid 
-            else:
-                left = mid + 1
-            
-        return right
-        
-        
-    def bfs(self, row, col, cur_max, heights):
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        que = deque([(row, col)])
-        visited = set()
-        
-        while que:
-            row, col = que.popleft()
+            if row == len(heights)-1 and col == len(heights[0])-1:
+                return cur_effort
             
             for i in directions:
                 n_r = row + i[0]
                 n_c = col + i[1]
                 
-                if 0 <= n_r < len(heights) and 0 <= n_c < len(heights[0]) and (n_r, n_c) not in visited and abs(heights[n_r][n_c]-heights[row][col]) <= cur_max:
-                    if n_r == len(heights)-1 and n_c == len(heights[0])-1:
-                        return True
-                    que.append((n_r, n_c))
-                    visited.add((n_r, n_c))
-            
-        return False
-            
+                if 0 <= n_r < len(heights) and 0 <= n_c < len(heights[0]):
+                    max_effort = max(cur_effort, abs(heights[row][col]-heights[n_r][n_c]))
+                    if all_efforts[n_r][n_c] > max_effort:
+                        
+                        all_efforts[n_r][n_c] = max_effort
+                        heapq.heappush(heap, (max_effort, n_r, n_c))
