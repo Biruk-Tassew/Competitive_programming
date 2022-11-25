8387c62b449fc3a3bba4de7be1aee378ab6b5226
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-25 22:21:48 +0300

    Time: 7378 ms (8.56%), Space: 15.7 MB (56.86%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,14 +1,8 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        abs_max = 0
-        abs_min = float('inf')
-        for row in range(len(heights)):
-            for col in range(len(heights[0])):
-                abs_min = min(abs_min, heights[row][col])
-                abs_max = max(abs_max, heights[row][col])
-                
+        
         left = 0
-        right = abs_max-abs_min
+        right = 10**6
         
         while left < right:
             mid = (left+right)//2
@@ -18,24 +12,25 @@ class Solution:
             else:
                 left = mid + 1
                 
-        return right
+        return left
                 
         
     def bfs(self, row, col, cur_max, heights):
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         que = deque([(row, col)])
-        visited = set()
+        visited = set([(row, col)])
         
         while que:
             row, col = que.popleft()
+            
+            if row == len(heights)-1 and col == len(heights[0])-1:
+                        return True
             
             for i in directions:
                 n_r = row + i[0]
                 n_c = col + i[1]
                 
                 if 0 <= n_r < len(heights) and 0 <= n_c < len(heights[0]) and abs(heights[n_r][n_c]-heights[row][col]) <= cur_max and (n_r, n_c) not in visited:
-                    if n_r == len(heights)-1 and n_c == len(heights[0])-1:
-                        return True
                     
                     visited.add((n_r, n_c))
                     que.append((n_r, n_c))
