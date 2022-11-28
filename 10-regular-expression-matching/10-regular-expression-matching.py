846f12e8ae23bbcd36b99455f4b1e8586fd1218e
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-28 15:25:02 +0300

    Time: 70 ms (80.28%), Space: 13.9 MB (91.67%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -2,10 +2,10 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         self.s = s
         self.p = p
-        self.memo = defaultdict(lambda:-1)
+        self.memo = defaultdict(lambda: -1)
         return self.solve(0, 0)
-    
-    def solve(self, p_idx, s_idx):
+
+    def solve(self, s_idx, p_idx):
         if p_idx == len(self.p):
             return len(self.s) == s_idx
         
@@ -15,9 +15,10 @@ class Solution:
         matching = s_idx < len(self.s) and self.p[p_idx] in {self.s[s_idx], "."}
         
         if p_idx < len(self.p)-1 and self.p[p_idx+1] == "*":
-            ans = (matching and self.solve(p_idx, s_idx+1) or (self.solve(p_idx+2, s_idx)))
+            ans = self.solve(s_idx, p_idx+2) or (matching and self.solve(s_idx+1, p_idx))
         else:
-            ans = matching and self.solve(p_idx+1, s_idx+1)
+            ans = matching and self.solve(s_idx+1, p_idx+1)
             
         self.memo[(s_idx, p_idx)] = ans
         return self.memo[(s_idx, p_idx)]
+        
