dbf7c5f69c407d61365f79af90eb00956cf74097
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-12-09 20:30:15 +0300

    Time: 151 ms (73.32%), Space: 19.8 MB (11.77%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,25 +1,20 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         self.directions = [(0, 1), (1, 0)]
-        self.memo = defaultdict(lambda:-1)
-        self.grid = grid
-        return self.solve(0, 0)
+        self.memo = defaultdict(lambda: -1)
+        self.grid = grid 
+        return self.dfs(0, 0)
     
-    def solve(self, row, col):
+    def dfs(self, row, col):
         if row == len(self.grid)-1 and col == len(self.grid[0])-1:
             return self.grid[row][col]
+        if row >= len(self.grid) or col >= len(self.grid[0]):
+            return float('inf')
         
         if self.memo[(row, col)] != -1:
             return self.memo[(row, col)]
         
-        res = float('inf')
-        for i in self. directions:
-            n_r = row + i[0]
-            n_c = col + i[1]
-            
-            if 0<=n_r<len(self.grid) and 0<=n_c<len(self.grid[0]):
-                res = min(res, self.solve(n_r, n_c)+self.grid[row][col])
-        
-        
+        res =  min(self.dfs(row+1, col), self.dfs(row, col+1)) +self.grid[row][col]
+    
         self.memo[(row, col)] = res 
         return self.memo[(row, col)]
