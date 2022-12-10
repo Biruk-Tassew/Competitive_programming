825d53bed21e7b449a0203299d0deacb4fa8338e
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-12-10 17:17:33 +0300

    Time: 33 ms (92.12%), Space: 13.9 MB (74.65%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,17 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        self.memo = defaultdict(lambda:-1)
-        return self.dfs(m-1, n-1)
-    
-    def dfs(self, row, col):
-        if row < 0 or col < 0:
-            return 0
-        if not row or not col:
-            return 1
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        dp[1][1] = 1
         
-        if self.memo[(row, col)] != -1:
-            return self.memo[(row, col)] 
-        
-        res = self.dfs(row-1, col) + self.dfs(row, col-1)
-        self.memo[(row, col)] = res
-        return self.memo[(row, col)] 
+        for row in range(1,m+1):
+            for col in range(1,n+1):
+                if row == 1 and col == 1:
+                    continue
+                dp[row][col] = dp[row-1][col]+dp[row][col-1]
+                
+        return dp[-1][-1]
