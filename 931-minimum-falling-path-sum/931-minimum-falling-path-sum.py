669eb2e231d94836fad7d9dec1303ac5af0b0025
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-12-13 14:37:04 +0300

    Time: 210 ms (74.12%), Space: 15.8 MB (20.46%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,22 +1,26 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        if len(matrix) == 1:
-            return min(matrix[0])
-        
-        dp = [[float('inf')]*(len(matrix[0])) for _ in range(len(matrix))]
-        dp[0] = matrix[0]
         min_sum = float('inf')
-        
-        for row in range(1, len(matrix)):
-            for col in range(len(matrix[0])):
-                if not col:
-                    dp[row][col] = min(dp[row][col], matrix[row][col]+dp[row-1][col], matrix[row][col]+dp[row-1][col+1])
-                elif col == len(matrix[0])-1:
-                    dp[row][col] = min(dp[row][col], matrix[row][col]+dp[row-1][col], matrix[row][col]+dp[row-1][col-1])
-                else:
-                    dp[row][col] = min(dp[row][col], matrix[row][col]+dp[row-1][col], matrix[row][col]+dp[row-1][col-1], matrix[row][col]+dp[row-1][col+1])
-                    
-                if row == len(matrix)-1:
-                    min_sum = min(min_sum, dp[row][col])
-                    
+        self.matrix = matrix
+        self.memo = defaultdict(lambda:"")
+        for i in range(len(matrix[0])):
+            min_sum = min(min_sum, self.solve(len(matrix)-1, i))
+            
         return min_sum
+    
+    def solve(self, row, col):
+        if col < 0 or col == len (self.matrix):
+            return float('inf')
+        if not row:
+            return self.matrix[row][col]
+        
+        if self.memo[(row, col)] != "":
+            return self.memo[(row, col)]
+        
+        left_dig = self.solve(row-1, col-1)
+        right_dig = self.solve(row-1, col+1)
+        up = self.solve(row-1, col)
+        
+        res = self.matrix[row][col] + min(left_dig, right_dig, up)
+        self.memo[(row, col)] = res
+        return self.memo[(row, col)]
