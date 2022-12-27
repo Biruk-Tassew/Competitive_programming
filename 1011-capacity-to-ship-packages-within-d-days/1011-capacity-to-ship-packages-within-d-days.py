e4d2acb38e5e3e2aa91d5e4a489edc294acbfafc
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-12-27 03:25:14 -0500

    Time: 1203 ms (55.91%), Space: 17.3 MB (8.09%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,32 +1,37 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        left = 0
-        right = sum(weights)+1
+        left = 1
+        right = sum(weights)
         self.weights = weights
         self.days = days
         
         while left < right:
-            mid = (left+right)//2
             
-            if self.shippable(mid):
-                right = mid 
+            mid = (right+left)//2
+            
+            if self.shipable(mid):
+                right = mid
             else:
                 left = mid + 1
                 
-        return left
+        return left 
+    
+    def shipable(self, ship_capacity):
+        cur_cap = ship_capacity
+        days = 0
+        idx = 0
         
-    def shippable(self, min_weight):
-        cur_left = min_weight
-        days = 1
-        
-        for i in self.weights:
-            if i > min_weight:
+        while idx < len(self.weights):
+            while idx < len(self.weights) and cur_cap - self.weights[idx] >= 0:
+                cur_cap -= self.weights[idx]
+                idx += 1
+            
+            days += 1
+            if self.days < days:
                 return False
-            
-            cur_left -= i
-            
-            if cur_left < 0:
-                cur_left = min_weight - i
-                days += 1
-                
-        return self.days >= days
+            cur_cap = ship_capacity
+        
+        
+        if self.days < days:
+            return False
+        return True
