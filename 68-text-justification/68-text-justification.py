8d22033c198aca93bb34ab092b39bc2e73fb3663
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-01-01 16:07:19 -0500

    Time: 34 ms (85.86%), Space: 13.9 MB (91.24%) - LeetHub

--- 68-text-justification/68-text-justification.py
+++ 68-text-justification/68-text-justification.py
@@ -5,24 +5,25 @@ class Solution:
         cur_len = 0
         
         for word in words:
-            if cur_len + len(word) + len(cur_list) > maxWidth:
+            if cur_len + len(cur_list) + len(word) > maxWidth:
                 if len(cur_list) == 1:
-                    res.append(cur_list[0]+" "*(maxWidth-cur_len))
+                    res.append(cur_list[0]+" "*(maxWidth-len(cur_list[0])))
                 else:
-                    spaces = maxWidth - cur_len
-                    space_btn = spaces//(len(cur_list)-1)
-                    space_extra = spaces%(len(cur_list)-1)
+                    spaces_to_add = maxWidth - cur_len
+                    in_btn_space = spaces_to_add // (len(cur_list)-1)
+                    extra_space = spaces_to_add % (len(cur_list)-1)
                     
-                    for i in range(space_extra):
+                    for i in range(extra_space):
                         cur_list[i] += " "
                         
-                    res.append((" "*space_btn).join(cur_list))
+                    res.append((" "*in_btn_space).join(cur_list))
+                    
                 cur_list = []
                 cur_len = 0
+                
             cur_list.append(word)
             cur_len += len(word)
             
-        
-        res.append(" ".join(cur_list)+" "*(maxWidth-cur_len-len(cur_list)+1))    
+        res.append(" ".join(cur_list)+" "*(maxWidth-cur_len-len(cur_list)+1))
         return res
                     
