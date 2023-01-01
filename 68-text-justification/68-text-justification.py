a8fdf5e5f5e375b8817cdbfab9d5dbae086fd943
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-01-01 16:17:59 -0500

    Time: 28 ms (96.00%), Space: 13.8 MB (91.24%) - LeetHub

--- 68-text-justification/68-text-justification.py
+++ 68-text-justification/68-text-justification.py
@@ -6,18 +6,10 @@ class Solution:
         
         for word in words:
             if cur_len + len(cur_list) + len(word) > maxWidth:
-                if len(cur_list) == 1:
-                    res.append(cur_list[0]+" "*(maxWidth-len(cur_list[0])))
-                else:
-                    spaces_to_add = maxWidth - cur_len
-                    in_btn_space = spaces_to_add // (len(cur_list)-1)
-                    extra_space = spaces_to_add % (len(cur_list)-1)
+                for i in range(maxWidth - cur_len):
+                    cur_list[i%(len(cur_list)-1 or 1)] += " "
                     
-                    for i in range(extra_space):
-                        cur_list[i] += " "
-                        
-                    res.append((" "*in_btn_space).join(cur_list))
-                    
+                res.append("".join(cur_list))
                 cur_list = []
                 cur_len = 0
                 
