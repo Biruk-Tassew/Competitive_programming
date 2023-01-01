4b65adf48900bad9ee4260750d533b8456435ada
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-01-01 06:44:43 -0500

    Time: 29 ms (91.56%), Space: 13.9 MB (22.42%) - LeetHub

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -1,18 +1,12 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        word_dict = {}
         s = s.split()
         
         if len(pattern) != len(s):
             return False
         
         for i in range(len(s)):
-            if pattern[i] in word_dict:
-                if word_dict[pattern[i]] != s[i]:
-                    return False
-            else:
-                if s[i] in word_dict.values():
-                    return False
-                word_dict[pattern[i]] = s[i]
-                
+            if pattern.index(pattern[i]) != s.index(s[i]):
+                return False
+            
         return True
