9f15b6c2167548b5caa0d29c5f0193efb3e79cc6
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-01-20 07:30:17 -0500

    Time: 241 ms (66.41%), Space: 22.4 MB (32.34%) - LeetHub

--- 491-non-decreasing-subsequences/491-non-decreasing-subsequences.py
+++ 491-non-decreasing-subsequences/491-non-decreasing-subsequences.py
@@ -1,24 +1,23 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         self.nums = nums
-        self.res = []
+        self.res = set()
         self.backtrack(0, [])
         
         return self.res
     
-    def backtrack(self, st_idx, cur_list):
-        if st_idx == len(self.nums):
-            if len(cur_list) > 1 and cur_list not in self.res:
-                self.res.append(cur_list.copy())
+    def backtrack(self, cur_idx, cur_list):
+        if cur_idx == len(self.nums):
+            if len(cur_list) > 1:
+                self.res.add(tuple(cur_list))
             return 
         
-        if len(cur_list) > 1 and cur_list not in self.res:
-            self.res.append(cur_list.copy())
         
-        for i in range(st_idx, len(self.nums)):
-            if not cur_list or self.nums[i] >= cur_list[-1]:
-                cur_list.append(self.nums[i])
-                
-                self.backtrack(i+1, cur_list)
-                
-                cur_list.pop()
+        if not cur_list or self.nums[cur_idx] >= cur_list[-1]:
+            cur_list.append(self.nums[cur_idx])
+            
+            self.backtrack(cur_idx+1, cur_list)
+            
+            cur_list.pop()
+            
+        self.backtrack(cur_idx+1, cur_list)
