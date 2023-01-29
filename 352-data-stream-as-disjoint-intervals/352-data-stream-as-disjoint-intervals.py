ac1c6897ba85b3e95ee663cf409b87fd95029c98
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-01-29 01:42:30 -0500

    Time: 156 ms (92.53%), Space: 18.9 MB (76.30%) - LeetHub

---
+++ 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
@@ -0,0 +1,28 @@
+class SummaryRanges:
+
+    def __init__(self):
+        self.intervals = [[float('-inf'), float('-inf')], [float('inf'), float('inf')]]
+        
+    def addNum(self, value: int) -> None:
+        i = bisect.bisect(self.intervals, [value])
+        ps, pe = self.intervals[i-1]
+        ns, ne = self.intervals[i]
+        
+        if pe == value - 1 and ns == value + 1:
+            self.intervals = self.intervals[:i-1] + [[ps, ne]] + self.intervals[i+1:]
+        elif pe == value - 1:
+            self.intervals[i-1][1] = value
+        elif ns == value + 1:
+            self.intervals[i][0] = value
+        elif pe < value - 1 and ns > value + 1:
+            self.intervals = self.intervals[:i] + [[value, value]] + self.intervals[i:]
+        
+
+    def getIntervals(self) -> List[List[int]]:
+        return self.intervals[1:-1]
+
+
+# Your SummaryRanges object will be instantiated and called as such:
+# obj = SummaryRanges()
+# obj.addNum(value)
+# param_2 = obj.getIntervals()
