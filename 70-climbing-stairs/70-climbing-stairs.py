5539fe7f674672e070ec0ed659e283559735c6ee
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-02-28 14:12:16 +0300

    Time: 31 ms (68.83%), Space: 14 MB (7.66%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,16 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        self.memo = defaultdict(lambda :-1)
-        return self.climb(n)
-    def climb(self, n):
+        return self.solve(n)
+    
+    @cache
+    def solve(self, n):
         if n <= 0:
             return 0
-        if n == 1 or n == 2:
+        if n <= 2:
             return n
         
-        if self.memo[n] != -1:
-            return self.memo[n]
-        
-        res = self.climb(n-1)+self.climb(n-2)
-        self.memo[n] = res
-        return self.memo[n]
+        return self.solve(n-1)+self.solve(n-2)
