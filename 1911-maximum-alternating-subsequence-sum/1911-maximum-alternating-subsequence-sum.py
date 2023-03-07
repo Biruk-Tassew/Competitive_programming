6bd0cda5fd257965f162e2f8ce14791288362a11
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-03-07 13:55:50 +0300

    Time: 3972 ms (5.59%), Space: 446.7 MB (5.13%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        self.memo = defaultdict(lambda: "")
-        return self.dfs(nums, 0, True)
+        self.nums = nums
+        return self.dfs(0, True)
     
-    def dfs(self, nums, cur_idx, isEven):
-        if cur_idx == len(nums):
+    @cache
+    def dfs(self, cur_idx, isEven):
+        if cur_idx == len(self.nums):
             return 0
+            
+        num = self.nums[cur_idx] if isEven else -self.nums[cur_idx]
         
-        if self.memo[(cur_idx, isEven)]:
-            return self.memo[(cur_idx, isEven)]
+        take = num + self.dfs(cur_idx+1, not isEven)
+        not_take = self.dfs(cur_idx+1, isEven)
         
-        num = nums[cur_idx] if isEven else -nums[cur_idx]
-        
-        take = num + self.dfs(nums, cur_idx+1, not isEven)
-        not_take = self.dfs(nums, cur_idx+1, isEven)
-        
-        self.memo[(cur_idx, isEven)] = max(take, not_take)
-        return self.memo[(cur_idx, isEven)]
+        return max(take, not_take)
