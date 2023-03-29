231bde2ba3f4fad9d6fca502acdaa5a422c5ed80
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-03-29 11:44:23 +0300

    Time: 35 ms (95.57%), Space: 14 MB (46.84%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        satisfaction.sort()
-        memo = defaultdict(lambda: "*")
-        return self.dfs(0, 1, satisfaction, memo)
-    def dfs(self, cur_idx, cur_dishes, sat, memo):
-        if cur_idx == len(sat):
-            return 0
+        satisfaction.sort(reverse=True)
+        cur_sum = 0
+        total = 0
         
-        if memo[(cur_idx, cur_dishes)] != "*":
-            return memo[(cur_idx, cur_dishes)] 
+        for i in range(len(satisfaction)):
+            cur_sum += satisfaction[i]
+            
+            if cur_sum < 0:
+                return total
+            
+            total += cur_sum
+            
+        return total
         
-        make_dish = sat[cur_idx]*cur_dishes + self.dfs(cur_idx+1, cur_dishes+1, sat, memo)
-        leave_dish =  self.dfs(cur_idx+1, cur_dishes, sat, memo)
-        
-        memo[(cur_idx, cur_dishes)] = max(0, make_dish, leave_dish)
-        return memo[(cur_idx, cur_dishes)] 
