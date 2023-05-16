5960ad092b0664df8b72125a19bd994957718014
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-05-16 22:41:16 +0300

    Time: 695 ms (16.07%), Space: 23.5 MB (21.72%) - LeetHub

---
+++ 463-island-perimeter/463-island-perimeter.py
@@ -0,0 +1,31 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        visited = set()
+        self.count = 0
+        directions = [(0,1),(1, 0),(-1,0),(0,-1)]
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col]:
+                    self.dfs(row, col, grid, visited, directions)
+                    return self.count
+        
+        
+    def dfs(self, row, col, grid, visited, directions):
+        visited.add((row, col))
+        cur_cnt = 0
+        for i in directions:
+            n_r = row + i[0]
+            n_c = col + i[1]
+            
+            if 0 <= n_r < len(grid) and 0 <= n_c < len(grid[0]) and (n_r, n_c) not in visited:
+                
+                if not grid[n_r][n_c]:
+                    cur_cnt += 1
+                elif grid[n_r][n_c] == 1:
+                    self.dfs(n_r, n_c, grid, visited, directions)
+            elif (n_r, n_c) not in visited:
+                cur_cnt += 1
+            
+        self.count += cur_cnt 
+                
+        
