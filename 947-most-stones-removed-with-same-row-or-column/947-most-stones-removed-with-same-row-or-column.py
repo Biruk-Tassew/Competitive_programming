143cb8ced271de81b1bbf7e5dffcbc03347c3fb5
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-07-12 21:20:22 +0300

    Time: 180 ms (57.48%), Space: 16.8 MB (99.76%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.py
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.py
@@ -1,19 +1,24 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         self.parents = {}
-        for i, j in stones:
-            self.parents.setdefault(i, i)
-            self.parents.setdefault(~j, ~j)
-            self.union(i, ~j)
-        return len(stones) - len(set([self.find(x) for x in self.parents]))
+        
+        for row, col in stones:
+            self.parents.setdefault(row, row)
+            self.parents.setdefault(str(col)+"c", str(col)+"c")
+            self.union(row, str(col)+"c")
+            
+        
+        return len(stones) - len(set([self.find(child) for child in self.parents]))
     
-    def find(self, x):
-        if x != self.parents[x]:
-            self.parents[x] = self.find(self.parents[x])
-        return self.parents[x]
-    
-    def union(self, x, y):
-        self.parents[self.find(x)] = self.find(y)
-
-    
-    
+    def find(self, child):
+        if child != self.parents[child]:
+            self.parents[child] = self.find(self.parents[child])
+        return self.parents[child]
+        
+    def union(self, ch_one, ch_two):
+        self.parents[self.find(ch_one)] = self.find(ch_two)
+        
+        
+        
+#     o x
+#     x o
