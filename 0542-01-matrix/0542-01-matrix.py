ef2a101613d09317d360dbe7aebb9e486dd29692
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-08-18 10:53:07 +0300

    Time: 587 ms (71.17%), Space: 20.3 MB (33.31%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,21 +1,32 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        dp = [[float('inf')]*(len(mat[0])+2) for _ in range(len(mat)+2)]
-        
-        for row in range(1, len(mat)+1):
-            for col in range(1, len(mat[0])+1):
+        ans = [[float('inf')]*len(mat[0]) for _ in range(len(mat))]
+        directions = [(0,1), (1,0), (0,-1), (-1,0)]
+        que = deque()
+        for row in range(len(mat)):
+            for col in range(len(mat[0])):
+                if not mat[row][col]:
+                    que.append((row, col))
+                    ans[row][col] = 0
                 
-                if not mat[row-1][col-1]:
-                    dp[row][col] = 0
-                else:
-                    dp[row][col] = min(dp[row][col], dp[row][col-1]+1, dp[row-1][col]+1)
+        
+        level = 0
+        seen = set()
+        while que:
+            
+            for _ in range(len(que)):
+                row, col = que.popleft()
+                
+                for i in directions:
+                    n_r = row+i[0]
+                    n_c = col+i[1]
                     
-        for row in range(len(mat), 0, -1):
-            for col in range(len(mat[0]), 0, -1):
-                if not mat[row-1][col-1]:
-                    dp[row][col] = 0
-                else:
-                    dp[row][col] = min(dp[row][col], dp[row][col+1]+1, dp[row+1][col]+1)
-        
-                
-        return [row[1:-1] for row in dp[1:-1]]
+                    if 0<=n_r<len(mat) and 0<=n_c<len(mat[0]) and ans[n_r][n_c] > ans[row][col] + 1:
+                        que.append((n_r, n_c))
+                        ans[n_r][n_c] = ans[row][col] + 1
+            level += 1
+            
+        return ans
+                        
+                    
+            
