c0c345c057a7ac44e9d77dc341e888c48a3228dd
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-08-21 22:36:03 +0300

    Time: 181 ms (76.79%), Space: 19.2 MB (51.86%) - LeetHub

---
+++ 0529-minesweeper/0529-minesweeper.py
@@ -0,0 +1,30 @@
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        self.directions = [(0,1),(1,0),(0,-1),(-1,0),(-1,1),(1,-1),(-1,-1),(1,1)]
+        
+        print(board[click[0]][click[1]]== 'M')
+        if board[click[0]][click[1]] == 'M':
+            board[click[0]][click[1]] = 'X'
+            return board
+        
+        self.dfs(board, click[0], click[1])
+        return board
+    
+    def dfs(self, board, row, col):
+        mines_count = 0
+        for i in range(8):
+            n_row = row + self.directions[i][0]
+            n_col = col + self.directions[i][1]
+            if 0 <= n_row < len(board) and 0 <= n_col < len(board[0]) and board[n_row][n_col] == 'M':
+                mines_count += 1
+                
+        if mines_count:
+            board[row][col] = str(mines_count)
+        else:
+            board[row][col] = 'B'
+            for i in range(8):
+                n_row = row + self.directions[i][0]
+                n_col = col + self.directions[i][1]
+                if 0 <= n_row < len(board) and 0 <= n_col < len(board[0]) and board[n_row][n_col] == 'E':
+                    self.dfs(board, n_row, n_col)
+                
